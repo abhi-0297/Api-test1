e789b4641fa0e8bd169a76858bfade9cbb763844
Abhishek sikarwar <46720878+abhi-0297@users.noreply.github.com>
2021-01-06 14:10:02 +0530

    Add files via upload

---
+++ Api/task2/routes/routes.js
@@ -0,0 +1,17 @@
+
+
+
+const userRoutes = require('./users');
+
+const appRouter = (app, fs) => {
+
+  app.get('/', (req, res) => {
+    res.send('welcome to the development api-server');
+  });
+
+  
+  userRoutes(app, fs);
+};
+
+// this line is unchanged
+module.exports = appRouter;
---
+++ Api/task2/server.js
@@ -0,0 +1,21 @@
+// load up the express framework and body-parser helper
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create an instance of express to serve our end points
+const app = express();
+
+
+const fs = require('fs');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const routes = require('./routes/routes.js')(app, fs);
+
+
+const server = app.listen(3000, () => {
+  console.log('listening on port %s...', server.address().port);
+});
